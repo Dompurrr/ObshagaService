0cd751b2f512dfb7857be88d2d6999f7adfb19ec
Denis Fominykh <mr-dew-fominyh@yandex.ru>
2022-07-14 21:50:40 +0300

    Spring and Vue.js init commit

---
+++ src/main/java/com/dompurrr/obshagaservice/ObshagaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dompurrr.obshagaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObshagaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObshagaServiceApplication.class, args);
+    }
+
+}
